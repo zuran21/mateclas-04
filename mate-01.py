1488931a148d39f947dfbf3cce33463296d9ed95
zuran21 <81603223+zuran21@users.noreply.github.com>
2021-05-07 23:39:55 -0500

    Create mate-01.py

---
+++ mate-01.py
@@ -0,0 +1,54 @@
+
+x = ("Hello wolrd")
+print("x")
+
+name = (input ("cual es tu nombre"))
+print ('hola', name)
+print ('hola' + name)
+
+
+x=((3+2)/(3*2))**2
+print (x)
+
+
+
+horas = int(input("numero de horas"))
+costo = int(input ("costo por hora"))
+paga = (horas*costo)
+
+print (("el pago es de:"), paga)
+
+
+
+
+
+n =int(input("numero entero"))
+s = ((n*(n+1))*0.5)
+print =(("la suma es :"),n )
+
+
+
+#nombre de usuario//////////////////////////
+
+
+nombre = input("nombre de usuario")
+numero = input("ingrese un numero:")
+print = (nombre*int(numero,))
+
+
+
+#ejercicio adicional de int e input//////////
+#ganancias de mensuales y totales
+
+gananciasdefebrero =int(input("ingresar ganancias del mes febrero:"))
+
+gananciasdemarzo = int(input("ingresar ganancias del mes marzo :"))
+
+gananciasdeabril = int(input("ingresar ganancias del mes abril :"))
+
+gananciasdemayo = int(input("ingresar ganancias del mes mayo :"))
+
+gananciasTotales = ((int(input("ingresar ganancias del mes enero :"))) + gananciasdefebrero + gananciasdemarzo + gananciasdeabril + gananciasdemayo)
+ 
+print( "las ganancias totales son :" ,gananciasTotales)
+
